ca6b966a0e3f40998a6990ea2ab55adf42dee287
Mayank Agarwal <mayank.agarwal2304@gmail.com>
2025-06-07 21:17:55 +0530

    third commit

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -3,10 +3,11 @@ import axios from 'axios';
 import { ADDNEW_TODO, GETALL_TODO, TOGGLE_TODO, UPDATE_TODO, DELETE_TODO, TOGGLE_TAB } from './type';
 
 const API_URL = 'https://vercel-backend-mayank.onrender.com';
+// const API_URL = "http://localhost:8000"
 
 export const addNewTodo = (data) => async (dispatch) => {
     try {
-        const res = await axios.post(`${API_URL}/todos`, { data });
+        const res = await axios.post(`${API_URL}/todos`, data);
 
         dispatch({ type: ADDNEW_TODO , payload: res.data });
     } catch (error) {
